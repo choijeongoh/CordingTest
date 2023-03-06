a7c27065c29bd2cdbcb2dc1fd58536f876a8ef9b
choijeongoh <119948141+choijeongoh@users.noreply.github.com>
2023-03-06 18:56:02 +0900

    [level 0] Title: 가위 바위 보, Time: 2.51 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static String solution(String rsp) {
+        String answer = "";
+        String[] array = rsp.split("");
+        for(int i = 0; i < rsp.length(); i++) {
+            if(array[i].equals("2")){
+                answer += "0";
+            } else if (array[i].equals("0")) {
+                answer += "5";
+            } else if (array[i].equals("5")) {
+                answer += "2";
+            }
+        }return answer;
+    }
+}
