2cb8d4439380fb728735802223000e46530d6eef
choijeongoh <119948141+choijeongoh@users.noreply.github.com>
2023-03-09 19:16:55 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.16 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,15 @@
+class Solution {
+     public static int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= 1000; i++) {
+            if(i * i == n) {
+                answer = 1;
+                break;
+            } else if (i * i != n) {
+                answer = 2;
+            }
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
